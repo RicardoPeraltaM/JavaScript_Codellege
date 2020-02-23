ae7e6a7fb77a1fbb5db0bca24d313371110ec704
SandovalJr <edubizzle7@gmail.com>
2020-02-23 14:16:25 -0600

    Funcionando bien

--- numero_inverso/numero_inverso.js
+++ numero_inverso/numero_inverso.js
@@ -8,15 +8,16 @@ function alrevez() {
   const alerta = document.getElementById("alerta");
 
   if (numero > 99 && numero < 1000) {
-    //   saco unidades
-    let unidades = numero %10;
     // coloco el math round para que el numero que salga lo redonde al valor mas cercano
     let centenas = Math.round(numero / 100);
+ 
+    //   saco unidades
+    let unidades = Math.round((numero %100) %10);
 
     // console.log(unidades);
     // console.log(centenas);
 
-    if (unidades == centenas) {
+    if (centenas == unidades) {
       alerta.innerHTML = `<div class="text-center alert alert-success mt-3">El numero es igual inverso</div>`;
     } else {
       alerta.innerHTML = `<div class="text-center alert alert-warning mt-3">El numero NO es igual inverso</div>`;
