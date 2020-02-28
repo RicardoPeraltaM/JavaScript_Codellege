95fa585203b0d3519fb0f1fac7bcb320eb12a3f5
SandovalJr <edubizzle7@gmail.com>
2020-02-27 23:19:19 -0600

    practica de carretera finalizada

--- JavaScript_Ciclos/Prompt/Carreteras/carretera.js
+++ JavaScript_Ciclos/Prompt/Carreteras/carretera.js
@@ -1,15 +1,25 @@
-
 eventListener();
 
-function eventListener (){
-     const boton = document.getElementById('boton');
-     boton.addEventListener('click',carretera_km);
+function eventListener() {
+  const boton = document.getElementById("boton");
+  boton.addEventListener("click", carretera_km);
 }
 
-function carretera_km (){
-zac =70;
-a = 150;
+function carretera_km() {
+  let ags = 70,
+    zacatecas = 150;
+  let carretera = true;
 
+  // mientras carretera sea true
+  while (carretera) {
+    ags++;
+    zacatecas--;
+    if (ags == zacatecas) {
+      carretera = false;
+    }
+  }
 
-
+  let alerta_1 = document.getElementById("alerta_1");
+  //   puedo imprimir ags o zacatecas igual dara el mismo resultado :)
+  alerta_1.innerHTML = `<div class="text-center alert alert-primary"> Se encontraron en el km ${zacatecas}</div>`;
 }
