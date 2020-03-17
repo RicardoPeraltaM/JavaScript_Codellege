6d4ac5dfe4837affd3efe2f396de3d29b73c23ee
SandovalJr <edubizzle7@gmail.com>
2020-03-17 11:16:19 -0600

    finalizado

--- Arrays/practica_4/practica4.js
+++ Arrays/practica_4/practica4.js
@@ -12,7 +12,7 @@ function CreatDom(event) {
     // Si es NAN no es un numero
     // Si nan es falso es un numero
     if (isNaN(event.target.value)) return;
-    console.log("works");
+    // console.log("works");
 
     rellenarArray(event.target.value);
   }
@@ -23,6 +23,22 @@ function rellenarArray(tamañoArray) {
 
   for (let x = 0; x < tamañoArray; x++) {
     array_n_aleatorios[x] = Math.floor(Math.random() * (10 - 1)) + 1;
-    console.log(array_n_aleatorios)
   }
+
+  //   console.log(array_n_aleatorios);
+  MostrarValores(array_n_aleatorios);
+}
+
+function MostrarValores(array_imprimir) {
+  console.log(array_imprimir+"Mostrar valores");
+  
+    let ZonePrint = document.getElementById("ZonePrint");
+    let suma_imprimir = document.getElementById("suma_imprimir");
+let suma_array = 0;
+  for (let x = 0; x < array_imprimir.length; x++) {
+      array_imprimir[x] = array_imprimir[x];
+      suma_array = suma_array + array_imprimir[x];
+  }
+  ZonePrint.innerText = array_imprimir;
+  suma_imprimir.innerHTML= `La suma es ${suma_array}`
 }
