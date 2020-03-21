0f7548309debff1ac8e1836d86ce5cf421f0aec5
SandovalJr <edubizzle7@gmail.com>
2020-03-21 16:26:35 -0600

    Se agregaron numeros pares e impares finalizado

--- Arrays/Numeros_Pares/numeros_pares.js
+++ Arrays/Numeros_Pares/numeros_pares.js
@@ -1,3 +1,5 @@
+EventListener();
+
 function EventListener() {
   document.getElementById("t_array").addEventListener("keypress", Validaciones);
 }
@@ -10,9 +12,32 @@ function Validaciones(validacion) {
 }
 
 function FillArray(tamaño_array) {
-let array = [];
-for (let x = 0; x < tamaño_array; x++){
-
+  console.log(tamaño_array);
+  let array = [];
+  for (let x = 0; x < tamaño_array; x++) {
+    array.push(Math.floor(Math.random() * (1000 - 1) + 1));
+  }
+  console.log(array);
+  N_pares(array);
 }
 
+function N_pares(arreglo) {
+  let arreglo_pares = [];
+  let arreglo_impares = [];
+
+  for (let i = 0; i < arreglo.length; i++) {
+    if (arreglo[i] % 2 == 0) {
+      arreglo_pares.push(arreglo[i]);
+    } else {
+      arreglo_impares.push(arreglo[i]);
+    }
+  }
+
+  //alertas
+  let n_par = document.getElementById("n_par");
+  let n_impar = document.getElementById("n_impar");
+
+  //  Imprimimos :
+  n_par.innerText = `Numeros pares: ${arreglo_pares.sort()}`;
+  n_impar.innerText = `Numeros impares: ${arreglo_impares.sort()}`;
 }
