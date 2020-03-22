5378d9ad1b0147b28244a55c60a0f7a02958f8cc
SandovalJr <edubizzle7@gmail.com>
2020-03-21 18:25:47 -0600

    finalizado

--- Arrays/Numeros_Primos/primos.js
+++ Arrays/Numeros_Primos/primos.js
@@ -6,20 +6,23 @@ function EventListener() {
 function FillArray() {
   let maximo_numero = 1000;
   let n_primo = [];
-  let ul = document.getElementById('lista_numeros_primos');
-  
-  for (let j = 1; j < maximo_numero; j++) {
-      if (Numero_primo(j)) {
-        let li = document.createElement('li');
-        li.setAttribute('class','form-group-item text-center')      
+  let ul = document.getElementById("lista_numeros_primos");
+
+  for (let j = maximo_numero; j > 0; j--) {
+    if (Numero_primo(j)) {
+      // Lo utilice asi para imprimirlo en lista
+      let li = document.createElement("li");
+      li.setAttribute("class", "form-group-item text-center");
       n_primo.push(j);
-      li.innerText= `Numero primo: ${j}`
+      li.innerText = `Numero primo: ${j}`;
       ul.appendChild(li);
     }
   }
-  console.log(n_primo);
-}
+  // a consola en orden decendente
+  console.log(n_primo.reverse());
 
+  // console.log(n_primo);
+}
 
 function Numero_primo(numero) {
   for (let i = 2; i < numero; i++) {
