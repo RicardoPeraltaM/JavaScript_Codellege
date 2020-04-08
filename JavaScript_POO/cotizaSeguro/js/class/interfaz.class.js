439f2ffa61e9d0fbb86b6c3ce12d45b41b588f86
SandovalJr <edubizzle7@gmail.com>
2020-04-08 09:39:47 -0500

    finalizar

---
+++ JavaScript_POO/cotizaSeguro/js/class/interfaz.class.js
@@ -0,0 +1,17 @@
+class Interfaz {
+  mostrarMensaje(mensaje, tipo) {
+    const div = document.createElement("div");
+
+    if (tipo == "error") {
+      div.classList.add("mensaje", "error");
+    } else {
+      div.classList.add("mensaje", "correcto");
+    }
+
+    div.innerHTML = `${mensaje}`;
+
+    document
+      .querySelector("#cotizar-seguro")
+      .insertBefore(div, document.querySelector(" .form-group"));
+  }
+}
---
+++ JavaScript_POO/cotizaSeguro/js/main/app.js
@@ -0,0 +1,41 @@
+const anio = document.getElementById("anio");
+
+EventListener();
+llenarSelectAnio();
+
+function EventListener() {
+  document
+    .querySelector("#cotizar-seguro")
+    .addEventListener("submit", CotizarSeguro);
+}
+
+function CotizarSeguro(event) {
+  // quitarle el enlace default
+  event.preventDefault();
+
+  const marcaSeleccionada = document.getElementById("marca").value;
+  const tipo = document.querySelector('input[name = "tipo"]:checked');
+  const valueAnio = anio.value;
+
+  const interfaz = new Interfaz();
+
+  if (marcaSeleccionada == "" || tipo == "" || valueAnio == "") {
+    interfaz.mostrarMensaje(
+      "Faltan datos , favor de revisar el formulario",
+      "error"
+    );
+  }
+}
+
+function llenarSelectAnio() {
+  const max = new Date().getFullYear();
+  const min = max - 20;
+
+  for (let i = max; i > min; i--) {
+    const option = document.createElement("option");
+    option.value = i;
+    option.innerHTML = i;
+
+    anio.appendChild(option);
+  }
+}
